7e5ffb096e6ba4661b202ea56a5bcb160025bbf5
Sahaj-IBM <139918147+Sahaj-IBM@users.noreply.github.com>
2024-02-22 16:10:46 -0500

    Update next.config.js
    Signed-off-by: Sahaj-IBM <139918147+Sahaj-IBM@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    distDir: 'build',
-    basePath: "/ce-solutions-hub",
-}
+const nextConfig = {}
 
 module.exports = nextConfig
